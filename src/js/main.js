344b175213a57bc7304e0f86b3c6191d9fba6cec
Maxim <maximleff8@gmail.com>
2024-12-18 21:38:00 +0200

    update

--- src/js/main.js
+++ src/js/main.js
@@ -16,6 +16,14 @@ barba.init({
 
 jQuery(document).ready(function ($) {
 	// $('input[type="tel"]').inputmask({ mask: '+38 (999) 999-99-99' });
-	
 
+	$('[btn-burger]').click(function(){
+		$(this).toggleClass('active');
+		
+		if($(this).hasClass('active')) {
+			$('[nav-menu]').addClass('open')
+		} else {
+			$('[nav-menu]').removeClass('open')
+		}
+	});
 });
