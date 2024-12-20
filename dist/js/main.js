c6892f864f1a25dcfee13ad41deb42a99146e250
Maxim <maximleff8@gmail.com>
2024-12-20 12:04:01 +0200

    update

--- dist/js/main.js
+++ dist/js/main.js
@@ -17,13 +17,31 @@ barba.init({
 jQuery(document).ready(function ($) {
 	// $('input[type="tel"]').inputmask({ mask: '+38 (999) 999-99-99' });
 
-	$('[btn-burger]').click(function(){
+	$('[btn-burger]').click(function () {
 		$(this).toggleClass('active');
-		
-		if($(this).hasClass('active')) {
+
+		if ($(this).hasClass('active')) {
 			$('[nav-menu]').addClass('open')
 		} else {
 			$('[nav-menu]').removeClass('open')
 		}
 	});
+
+	$('.online-courses__tab').click(function () {
+		if (!$(this).hasClass('active')) {
+			$('.online-courses__tab.active').removeClass('active')
+			$(this).addClass('active')
+
+			$('.online-courses__tab .online-courses__tab-content').slideUp(200);
+			$(this).find('.online-courses__tab-content').slideDown(200, function () {
+				$(this).css({
+					display: "flex"
+				})
+			});
+
+			let index = $(this).attr('tab-text-index')
+			$(`.online-courses__image`).removeClass('active')
+			$(`.online-courses__image[tab-image-index=${index}]`).addClass('active')
+		}
+	})
 });
