e9d9ec2efe2f5c378698948a0e41dccc66e35f7c
Maxim <maximleff8@gmail.com>
2025-01-05 03:21:15 +0200

    home page update

--- dist/js/main.js
+++ dist/js/main.js
@@ -3,14 +3,14 @@ barba.init({
 		name: 'opacity-transition',
 		leave(data) {
 			return gsap.to(data.current.container, {
-				opacity: 0
+				opacity: 0,
 			});
 		},
 		enter(data) {
 			return gsap.from(data.next.container, {
-				opacity: 0
+				opacity: 0,
 			});
-		}
+		},
 	}],
 });
 
@@ -21,51 +21,23 @@ jQuery(document).ready(function ($) {
 		$(this).toggleClass('active');
 
 		if ($(this).hasClass('active')) {
-			$('[nav-menu]').addClass('open')
+			$('[nav-menu]').addClass('open');
+			$('body').addClass('overflow-hidden');
 		} else {
-			$('[nav-menu]').removeClass('open')
+			$('[nav-menu]').removeClass('open');
+			$('body').removeClass('overflow-hidden');
 		}
 	});
 
-	$('.online-courses__tab').click(function () {
-		if (!$(this).hasClass('active')) {
-			$('.online-courses__tab.active').removeClass('active')
-			$(this).addClass('active')
-
-			$('.online-courses__tab .online-courses__tab-content').slideUp(200);
-			$(this).find('.online-courses__tab-content').slideDown(200, function () {
-				$(this).css({
-					display: "flex"
-				})
-			});
-
-			let index = $(this).attr('tab-text-index')
-			$(`.online-courses__image`).removeClass('active')
-			$(`.online-courses__image[tab-image-index=${index}]`).addClass('active')
-		}
-	})
-
-	$('.faq__tab').click(function() {
-		if(!$(this).hasClass('active')){
-			$('.faq__tab.active').removeClass('active')
-			$(this).addClass('active')
-
-			$('.faq__tab .faq__tab-content').slideUp(200);
-			$(this).find('.faq__tab-content').slideDown(200, function () {
-				$(this).css({
-					display: "flex"
-				})
-			});
-		}
-	})
+	
 
 	const howLong = new Swiper('.how-long__slides', {
 		slidesPerView: 1,
 		spaceBetween: 15,
 
 		navigation: {
-			nextEl: '.swiper-button-next',
-			prevEl: '.swiper-button-prev',
+			nextEl: '.how-long__slides .swiper-button-next',
+			prevEl: '.how-long__slides .swiper-button-prev',
 		},
 		breakpoints: {
 			500: {
@@ -88,8 +60,8 @@ jQuery(document).ready(function ($) {
 		spaceBetween: 15,
 
 		navigation: {
-			nextEl: '.swiper-button-next',
-			prevEl: '.swiper-button-prev',
+			nextEl: '.our-teachers__slides .swiper-button-next',
+			prevEl: '.our-teachers__slides .swiper-button-prev',
 		},
 		breakpoints: {
 			500: {
@@ -106,4 +78,117 @@ jQuery(document).ready(function ($) {
 			},
 		},
 	});
+
+	const courses = new Swiper('.courses__slider', {
+		slidesPerView: 'auto',
+		spaceBetween: 15,
+		centeredSlides: true,
+
+		navigation: {
+			nextEl: '.courses__slider .swiper-button-next',
+			prevEl: '.courses__slider .swiper-button-prev',
+		},
+		initialSlide: 1,
+		breakpoints: {
+			1281: {
+				slidesPerView: 'auto',
+				// centeredSlides: false,
+			},
+		},
+	});
+
+	if ($(window).width() > 768) {
+		const scene = $('#parallax')[0];
+		if (scene) {
+			const parallaxInstance = new Parallax(scene);
+		}
+	}
+
+	$('.btn--list').click(function () {
+		$(this).toggleClass('active');
+	});
+
+	$(document).mouseup(function (e) {
+		const btn = $('.btn--list');
+
+		if (!btn.is(e.target)) {
+			btn.removeClass('active');
+		}
+	});
+
+	$('.faq__tab').click(function () {
+		if (!$(this).hasClass('active')) {
+			$('.faq__tab.active').removeClass('active');
+			$(this).addClass('active');
+
+			$('.faq__tab .faq__tab-content').slideUp(200);
+			$(this).find('.faq__tab-content').slideDown(200, function () {
+				$(this).css({
+					display: 'flex',
+				});
+			});
+		}
+	});
+
+	// online-courses
+	function changeTabCourses(tab, index) {
+		$('.online-courses__tab').removeClass('active');
+		tab.classList.add('active');
+		$('.online-courses__image.active').removeClass('active');
+		$(`.online-courses__col [tab-image-index=${index + 1}]`).addClass('active');
+	}
+
+	if (window.innerWidth > 1280) {
+		gsap.registerPlugin(ScrollTrigger);
+
+		const tl = gsap.timeline({
+			scrollTrigger: {
+				trigger: '.online-courses',
+				start: '-20% center',
+				end: '150% center',
+				scrub: 1,
+				// markers: true,
+				// pin: true,
+				snap: {
+					snapTo: [0.1, 0.35, 0.62, 0.9],
+					// duration: 0.2,
+					// delay: 0.2,
+					// ease: false
+				},
+			},
+		});
+		const tabs = document.querySelectorAll('.online-courses__tab');
+		tabs.forEach((tab, index) => {
+			tl.addLabel(`show-${index}`)
+				.from(tab.querySelector('.online-courses__tab-content-wrap'), {
+					height: 0,
+					opacity: 0,
+				})
+				.add(function () {
+					changeTabCourses(tab, index);
+				});
+			tl.addLabel(`hide-${index}`)
+				.to(tab.querySelector('.online-courses__tab-content-wrap'), {
+					height: 0,
+					opacity: 0,
+				}, '+=0.5')
+				.add(function () {
+					changeTabCourses(tab, index);
+				});
+		});
+	} else {
+		$('.online-courses__tab').click(function () {
+			if (!$(this).hasClass('active')) {
+				$('.online-courses__tab.active').removeClass('active');
+				$(this).addClass('active');
+
+				$('.online-courses__tab .online-courses__tab-content-wrap').slideUp(200);
+				$(this).find('.online-courses__tab-content-wrap').slideDown(200);
+
+				const index = $(this).attr('tab-text-index');
+				$('.online-courses__image').removeClass('active');
+				$(`.online-courses__image[tab-image-index=${index}]`).addClass('active');
+			}
+		});
+	}
 });
